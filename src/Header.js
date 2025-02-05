81b0d374131b1dfe45e3f4b4432f95863d140a6c
Vishal <vishal198shetty@gmail.com>
2025-02-05 11:08:40 +0530

    Add inline styles to Header component for background and link customization

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,29 @@ import React from 'react'
 
 const Header = () => {
   return (
+    <>
+    <style>
+      {
+        `
+        body{
+        background-color:lightblue;
+        }
+       
+        a{
+          text-decoration:none;
+          color:white;
+        }
+       
+        `
+      }
+    </style>
     <div style={{width:100+"%",textAlign:"center",height:50+"px",backgroundColor:"blue"}}>
        < a href='/'>Home</a>
         <a href='/profile'>Profile</a>
         <a href='/table'>Table</a>
         <a href='/account'>Account</a>
     </div>
+    </>
   )
 }
 
